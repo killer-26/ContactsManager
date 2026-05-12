ef66a6ec064098283473b684156323f46495cbb1
Votre-Mian-Adian-William <S4-POSTE133@Sinfo4.pigierci.ci>
2026-05-12 18:48:32 +0000

    modification des numeros

--- src/Main.java
+++ src/Main.java
@@ -8,31 +8,31 @@ public class Main {
         // Contact 1
         Contact contact1 = new Contact();
         contact1.name = "Jean";
-        contact1.phoneNumber = "0707070707";
+        contact1.phoneNumber = "0750248007";
         myContactsManager.addContact(contact1);
 
         // Contact 2
         Contact contact2 = new Contact();
         contact2.name = "Marc";
-        contact2.phoneNumber = "0101010101";
+        contact2.phoneNumber = "0101218078";
         myContactsManager.addContact(contact2);
 
         // Contact 3
         Contact contact3 = new Contact();
         contact3.name = "Foto";
-        contact3.phoneNumber = "0505050505";
+        contact3.phoneNumber = "0505608019";
         myContactsManager.addContact(contact3);
 
         // Contact 4
         Contact contact4 = new Contact();
         contact4.name = "Odilonne";
-        contact4.phoneNumber = "0706060606";
+        contact4.phoneNumber = "0701205425";
         myContactsManager.addContact(contact4);
 
         // Contact 5
         Contact contact5 = new Contact();
         contact5.name = "Cheick";
-        contact5.phoneNumber = "0709090909";
+        contact5.phoneNumber = "0752892014";
         myContactsManager.addContact(contact5);
 
         // Recherche d’un contact
